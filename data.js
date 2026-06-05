67ec6d291faba27cf4b099477e645d1a6860f50c
ramesh-madara <rameshmadara@gmail.com>
2026-06-05 22:29:55 +0530

    Create favicon and add to index.html; reset feedback count

--- data.js
+++ data.js
@@ -2,8 +2,7 @@ const CV_CONFIG = {
   totalCVs: 2,
   cvFolder: 'cvs/',
   cvExt: '.jpg',
-  totalFeedbacks: 2,
+  totalFeedbacks: 0,
   feedbackFolder: 'customerFeedback/',
   feedbackExt: '.jpg',
 };
-
