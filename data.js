1479905ce8f1e7d5706cb13bbfc876840f85a5b9
ramesh-madara <rameshmadara@gmail.com>
2026-06-05 21:22:27 +0530

    Update feedback count configuration

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const CV_CONFIG = {
   totalCVs: 2,
   cvFolder: 'cvs/',
   cvExt: '.jpg',
-  totalFeedbacks: 0,
+  totalFeedbacks: 2,
   feedbackFolder: 'customerFeedback/',
   feedbackExt: '.jpg',
 };
